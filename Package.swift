55d59f2d68931c64c9248356e7acf6e9170d30da
viachpaliy <viachpaliy@ukr.net>
2018-06-17 10:33:22 +0300

    change repository from original to fork

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "PangoCairo",
     dependencies: [
-        .Package(url: "https://github.com/rhx/SwiftCairo.git", majorVersion: 2),
-        .Package(url: "https://github.com/rhx/SwiftPango.git", majorVersion: 2)
+        .Package(url: "https://github.com/viachpaliy/SwiftCairo.git", majorVersion: 2),
+        .Package(url: "https://github.com/viachpaliy/SwiftPango.git", majorVersion: 2)
     ],
     swiftLanguageVersions: [3]
 )
