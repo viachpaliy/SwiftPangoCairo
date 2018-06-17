a02be7292e410c492bfce6d425d11d119e5b1498
viachpaliy <viachpaliy@ukr.net>
2018-06-17 14:41:32 +0300

    change version

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "PangoCairo",
     dependencies: [
-        .Package(url: "https://github.com/viachpaliy/SwiftCairo.git", majorVersion: 2),
-        .Package(url: "https://github.com/viachpaliy/SwiftPango.git", majorVersion: 2)
+        .Package(url: "https://github.com/viachpaliy/SwiftCairo.git",  majorVersion: 1),
+        .Package(url: "https://github.com/viachpaliy/SwiftPango.git",  majorVersion: 1)
     ],
     swiftLanguageVersions: [3]
 )
